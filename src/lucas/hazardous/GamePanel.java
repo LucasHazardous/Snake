9ab1bc0556db0e17fce68d11b5b8362da5212541
Adam W <developeradamw@gmail.com>
2022-02-01 20:14:22 +0100

    init essential variables and functions

--- src/lucas/hazardous/GamePanel.java
+++ src/lucas/hazardous/GamePanel.java
@@ -1,17 +1,74 @@
 package lucas.hazardous;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+import java.util.Timer;
 
 public class GamePanel extends JPanel implements ActionListener {
+
+    private static final int SCREEN_WIDTH = 500;
+    private static final int SCREEN_HEIGHT = 500;
+    private static final int UNIT_SIZE = 25;
+    private static final int GAME_UNITS = (SCREEN_HEIGHT*SCREEN_WIDTH)/UNIT_SIZE;
+    private static final int DELAY = 50;
+    private final int x[] = new int[GAME_UNITS];
+    private final int y[] = new int[GAME_UNITS];
+    private int bodyParts = 5;
+    private int applesEaten = 0;
+    private int appleX;
+    private int appleY;
+    private char direction = 'R';
+    private boolean running = false;
+    private Timer timer;
+    private Random random;
 
     GamePanel() {
 
     }
 
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver() {
+
+    }
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
 }
